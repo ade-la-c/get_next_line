33f9115ebdadfa2e0369fa330b0f103d81c1ed60
Adrian De la colina <ade-la-c@w-r5-p3.s19.be>
2020-02-15 22:03:40 +0100

    gnl working, scripts still fail tho

--- get_next_line.c
+++ get_next_line.c
@@ -5,67 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ade-la-c <ade-la-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/30 15:07:43 by ade-la-c          #+#    #+#             */
-/*   Updated: 2020/02/12 04:03:20 by ade-la-c         ###   ########.fr       */
+/*   Created: 2020/02/11 13:36:06 by ade-la-c          #+#    #+#             */
+/*   Updated: 2020/02/15 21:41:08 by ade-la-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-static char		*ft_crop_line(char **str)
+static int			ft_checker(char *str)
 {
-	char		*s1;
-	int			i;
-	int			len;
-
-	i = 0;
-	len = 0;
-	if (!*str)
-		return (NULL);
-	while (*str[len] && *str[len] != '\n')
-		len++;
-	if (!(s1 = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	while (i < len)
-	{
-		s1[i] = *str[i];
-		i++;
-	}
-	s1[i] = '\0';
-	if (!(*str = ft_substr(*str, len + 1, ft_strlen(*str) - len + 1)))
-		return (NULL);
-	return (s1);
-}
-
-static char		*ft_separate_line(char *buf, char **str)
-{
-	char		*s1;
-	int			i;
-	int			len;
-
-	i = 0;
-	len = 0;
-	s1 = NULL;
-	while (buf[len] && buf[len] != '\n')
-		len++;
-	if (!(s1 = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	while (i < len)
-	{
-		s1[i] = buf[i];
-		i++;
-	}
-	s1[i] = '\0';
-	i++;
-	if (!(*str = ft_strdup(&buf[i])))
-		return (NULL);
-	return (s1);
-}
-
-static int		ft_checker(char *str)
-{
-	size_t		i;
+	size_t			i;
 
 	i = -1;
 	if (!str)
@@ -73,66 +22,68 @@ static int		ft_checker(char *str)
 	while (str[++i])
 		if (str[i] == '\n')
 		{
-			if (i < ft_strlen(str))
+			if (i < ft_strlen(str) - 1)
 				return (2);
 			return (1);
 		}
 	return (0);
 }
 
-/*
-int				get_next_line(int fd, char **line)
+static char			*ft_split_at_newline(char **str, char *s2)
 {
-	static char		*str[FD_SIZE];
-	char			buf[BUFFER_SIZE < 0 ? 0 : BUFFER_SIZE + 1];
-	char			*tmp;
+	char			*ret;
+	char			*s1;
 	int				i;
-	int				ret;
+	int				len;
 
 	i = 0;
-	str[fd] = NULL;
-	tmp = NULL;
-	if (fd < 0 || fd > OPEN_MAX || !line || !BUFFER_SIZE)
-		return (-1);
-	if (read(fd, buf, BUFFER_SIZE) == 0)
-		return (0);
-	if (ft_checker(str[fd]) != -1)
-		ft_crop_line(&str[fd]);
-	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		buf[ret] = '\0';
-		if (ft_checker(buf) != -1)
-			break ;
-		else
-			ft_strjoin(tmp, buf);
-	}
-	ft_separate_line(buf, &str[fd]);
-	return (1);
+	len = 0;
+	s1 = (char *)*str;
+	//write(1, "x\n", 2);
+	while (s2[len] && s2[len] != '\n')
+		len++;//write(1, "a\n", 2);
+	if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);//write(1, "b\n", 2);
+	ret[len] = '\0';
+	while (i < len)
+	{//write(1, "c\n", 2);
+		ret[i] = s2[i];
+		i++;
+	}//write(1, "d\n", 2);
+	if (!(s1 = ft_substr(s2, len + 1, ft_strlen(s2) - len - 1)))
+		return (NULL);//write(1, "e\n", 2);
+	*str = s1;//write(1, "f\n", 2);
+	return (ret);
 }
-*/
-int				get_next_line(int fd, char **line)
+
+int					get_next_line(int fd, char **line)
 {
 	static char		*str[OPEN_MAX];
 	char			buf[BUFFER_SIZE < 0 ? 0 : BUFFER_SIZE + 1];
 	char			*tmp;
-	int				i;
 	int				ret;
 
-	i = 0;
-	str[fd] = NULL;
+	ret = 0;
 	tmp = NULL;
-	if (fd < 0 || fd > OPEN_MAX || !line || !BUFFER_SIZE)
+	if (fd < 0 || fd > OPEN_MAX || !line || BUFFER_SIZE < 1)
 		return (-1);
-	if (ft_checker(str[fd]) < 0)
-	{
-		tmp = ft_crop_line(&str[fd]);
+	buf[BUFFER_SIZE] = '\0';
+	if (ft_checker(str[fd]) > 0)
+	{//printf("appel1\n");
+		tmp = ft_split_at_newline(&str[fd], str[fd]);//printf("finappel1\n");
+		*line = tmp;
+		return (1);
 	}
-	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0 && ft_checker(str[fd]) == -1)
+	else if (ft_checker(str[fd]) == 0)
 	{
-		tmp = ft_strjoin(tmp, str[fd]);
-		if (ft_checker(buf) < 0)
-		{
-			ft_separate_line(buf, &str[fd]);
+		tmp = ft_strdup(str[fd]);
+		str[fd] = NULL;
+	}
+	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		if (ft_checker(buf) > 0)
+		{//printf("appel2\n");
+			tmp = ft_strjoin(tmp, ft_split_at_newline(&str[fd], buf));//printf("finappel2\n");
 			break ;
 		}
 		else
@@ -141,19 +92,20 @@ int				get_next_line(int fd, char **line)
 	*line = tmp;
 	return (ret == 0 ? 0 : 1);
 }
-
+/*
 int					main(void)
 {
-	int		fd = open("pseudocode", O_RDONLY);
+	int		fd = open("oui.txt", O_RDONLY);
 	char	*line;
 	int		ret;
 	while ((ret = get_next_line(fd, &line)) == 1)
 	{
 		printf("%d|%s\n", ret, line);
-//		free(line);
+		free(line);
 	}
 	printf("%d|%s\n", ret, line);
-//	free(line);
+	free(line);
 	close(fd);
 	return (0);
 }
+*/
